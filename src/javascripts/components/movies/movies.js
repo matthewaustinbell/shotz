d7e80ea4a57c0a0846aa323abd72e45d2c0c1096
Matt Bell <matthewaustinbell@gmail.com>
2019-04-22 18:55:54 -0500

    added json

--- src/javascripts/components/movies/movies.js
+++ src/javascripts/components/movies/movies.js
@@ -9,6 +9,17 @@ const domstringBuilder = () => {
   let domString = '';
   movies.forEach((movie) => {
     domString += `<h3>${movie.name}</h3>`;
+    domString += '<div class="card" style="width: 18rem;">';
+    domString += `<div id=${movie.id} class="card-body">`;
+    domString += `<h5 class="card-title">${movie.id}</h5>`;
+    domString += `<h5 class="card-title">${movie.name}</h5>`;
+    domString += `<h5 class="card-title">${movie.genre}</h5>`;
+    domString += `<h5 class="card-title">${movie.releaseDate}</h5>`;
+    domString += `<h5 class="card-title">${movie.description}</h5>`;
+    domString += `<h5 class="card-title">${movie.locations.length}</h5>`;
+    domString += '<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>';
+    domString += '</div>';
+    domString += '</div>';
   });
   util.printToDom('movies', domString);
 };
