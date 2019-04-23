897f0d7f522e5df149a55a9f3d50dd391b2d35d6
Matt Bell <matthewaustinbell@gmail.com>
2019-04-22 19:30:59 -0500

    wip locations

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -5,13 +5,38 @@ import './locations.scss';
 
 let locations = [];
 
+const shootTimeClass = (shootTime) => {
+  let selectedClass = ''; 
+  switch(shootTime){
+    case 'Morning':
+      selectedClass = 'bg-secondary'; 
+      break;
+      case 'Afernoon':
+      selectedClass = 'bg-success'; 
+      break;
+      case 'Evening':
+      selectedClass = 'bg-info'; 
+      break;
+      case 'After Dark':
+      selectedClass = 'bg-danger'; 
+      break;
+  }
+  return selectedClass; 
+}
+
 const domstringBuilder = () => {
   let domString = '';
   locations.forEach((location) => {
-    domString += `<h3>${location.name}</h3>`;
+    domString += '<div class="card" style="width: 18rem;">';
+    domString += `<h5 class="card-header${shootTimeClass(location.shootTime)}">${location.name}</h5>
+    domString += '<div class = 'card-body">; 
+    domString += `<img src="${location.imageUrl}" alt="Card image cap">`;
+    domString += `<h3>${location.address}</h3>`;
+    domString += '</div>';
   });
   util.printToDom('locations', domString);
 };
+
 
 const initializeLocations = () => {
   locationsData.getLocationsData()
